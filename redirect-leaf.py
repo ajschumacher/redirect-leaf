fad1db6ba0310725e8c670c3646ac10d5e76c638
Aaron Schumacher <ajschumacher@gmail.com>
2012-06-05 13:57:15 -0400

    state as of last night

--- redirect-leaf.py
+++ redirect-leaf.py
@@ -1,9 +1,13 @@
 import webapp2, json, os
 from google.appengine.api import urlfetch
 from google.appengine.ext.webapp import template
+from google.appengine.ext import db
+
+# going to need a model up here...
 
 class RootResponse(webapp2.RequestHandler):
     def get(self):
+        # all this needs to be moved/redone!
         response_data = {'score': 1}
         i_data = self.request.get_all('i')
         try:
@@ -19,8 +23,20 @@ class RootResponse(webapp2.RequestHandler):
             template_values = { 'data_url': data_url }
         except:
             template_values = {}
-        path = os.path.join(os.path.dirname(__file__), 'leaf.html')
+        path = os.path.join(os.path.dirname(__file__), 'interface.html')
         self.response.out.write(template.render(path, template_values))
+    def post(self):
+        # make new redirect objects via a json interface here
+        self.response.out.write('POST!')
 
-app = webapp2.WSGIApplication([('/', RootResponse)],
+class DoRedirect(webapp2.RequestHandler):
+    def get(self, redirect_key):
+        # get redirect location
+        # send report request to d if it exists
+        # redirect (not to this, but...)
+        self.redirect(redirect_key, permanent=True)
+        # hmm... note that this fails with a redirect loop for dumb values
+
+app = webapp2.WSGIApplication([('/', RootResponse),
+                               (r'/go/(.+)', DoRedirect)],
                               debug=True)
